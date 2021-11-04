03f212ecef45f40da66271527c36a85aa25b416a
Josef <josefhlava@yahoo.co.uk>
2021-11-04 15:17:16 +0100

    Created responsive header

---
+++ script.js
@@ -0,0 +1,8 @@
+function mobileNav() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+}
